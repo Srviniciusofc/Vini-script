06a324f5a0cae3183ab2145eb5cdb24730c756c8
SrVinicius <mv6969375@gmail.com>
2025-11-21 16:16:56 -0300

    update meu script.lua

--- meu script.lua
+++ meu script.lua
@@ -4,10 +4,8 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jen
 
 local Window = OrionLib:MakeWindow({Name = "Vini Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Vini v1"})
 
-
-
 local main = Window:MakeTab({
-	Name = "Main",
+	Name = "Tab 1",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
@@ -15,8 +13,42 @@ local main = Window:MakeTab({
 
 
 main:AddSection({
-	Name = "Fly"
+	Name = "Inicio"
 })
+
+main:AddButton({
+	Name = "Noclip",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+local Noclip = nil
+local Clip = nil
+
+function noclip()
+	Clip = false
+	local function Nocl()
+		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+					v.CanCollide = false
+				end
+			end
+		end
+		wait(0.21) -- basic optimization
+	end
+	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
+end
+
+function clip()
+	if Noclip then Noclip:Disconnect() end
+	Clip = true
+end
+
+noclip() -- to toggle noclip() and clip()
+
+
+
 
 
 

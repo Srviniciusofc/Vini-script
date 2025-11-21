de1ecdddc9c5cee45e323f91379965d5e68f475f
SrVinicius <mv6969375@gmail.com>
2025-11-21 16:53:00 -0300

    update meu script.lua

--- meu script.lua
+++ meu script.lua
@@ -509,8 +509,23 @@ end)
 
 
 main:AddButton({
-	Name = "sei la",
+	Name = "esp",
 	Callback = function()
       		print("button pressed")
   	end    
 })
+
+local players = game.Players:GetChildren()
+
+for _, player in pairs(players) do
+    if player.Name ~= game.Players.LocalPlayer.Name then
+        if player.Humanoid then
+            if not player:FindFirstChild("Highlight") then
+                local highlight = Instance.new('Highlight')
+                highlight.Parent = player
+                highlight.FaceColor = Color3.new(255, 255, 255) -- Change this to the desired color
+                highlight.Transparency = 0.5 -- Change this to the desired transparency
+            end
+        end
+    end
+end

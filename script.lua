7de6a08aeca72029f29e4db435f46c0e8ece928f
Vinícius <mv6969375@gmail.com>
2025-12-07 12:31:52 -0300

    update script.lua

--- script.lua
+++ script.lua
@@ -7,6 +7,27 @@ local Window = Library:MakeWindow({
 })
 
 
+
+--teste
+
+
+local WingImage = Library:CreateUI({
+    Name = "WingIMG",
+    Type = "Image",
+    Image = "rbxassetid://3926305904",
+    
+    Size = UDim2.new(0, 90, 0, 90),
+    Position = UDim2.new(0.92, 0, 0.12, 0), -- canto direito igual o da foto
+    Draggable = false
+})
+
+
+
+
+
+
+
+
 local Minimizer = Window:NewMinimizer({
   KeyCode = Enum.KeyCode.LeftControl
 })
